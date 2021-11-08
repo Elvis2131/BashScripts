9b5244d30f3854ee9e1984c7cf5b7a9817ae824d
elartey2131 <elartey2131@gmail.com>
2021-11-08 09:54:35 +0000

    Added the docker setup

--- others.sh
+++ others.sh
@@ -1,16 +1,8 @@
-sudo hostnamectl set-hostname <<name-of-machine>>
-sudo yum update -y && sudo yum upgrade -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo yum install epel-release java-11-openjdk-devel
-sudo yum install jenkins -y
-sudo systemctl daemon-reload
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
-sudo firewall-cmd --reload
-cat /var/lib/jenkins/secrets/initialAdminPassword
+# sleep 30 &
+# process_id=$!
+# echo "PID: $process_id"
+# wait $process_id
+# echo "Exit status: $?"
 
 
 ##### Installing Docker ######
